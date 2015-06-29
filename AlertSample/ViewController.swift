354df7eff057e7d16c82cec5a6d0c74a6105e032
suzuki_kiwamu <suzuki_kiwamu@cyberagent.co.jp>
2015-06-29 20:15:22 +0900

    Initial Commit

---
+++ AlertSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlertSample
+//
+//  Created by suzuki_kiwamu on 2015/06/29.
+//  Copyright (c) 2015年 suzuki_kiwamu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
